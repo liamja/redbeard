0a76313ea29c92e76c397f4498cc4b28c43e4202
Liam Anderson <Liam.Anderson.91@gmail.com>
2013-08-03 01:02:32 +0100

    Check info_hash for correct length. Included URI for escaping.

--- lib/redbeard/http.rb
+++ lib/redbeard/http.rb
@@ -1,7 +1,12 @@
 require 'sinatra'
+require 'uri'
 
 get '/announce' do
-    
+    unless params['INFO_HASH'].nil?
+        if params['INFO_HASH'].length != 40 
+            raise 'INFO_HASH is not 20 bytes long.'
+        end
+    end
 end
 
 get '/scrape' do
--- spec/redbeard_spec.rb
+++ spec/redbeard_spec.rb
@@ -1,6 +1,7 @@
 require 'redbeard'
 require 'rspec'
 require 'rack/test'
+require 'uri'
 
 ENV['RACK_ENV'] = 'test'
 
@@ -25,6 +26,12 @@ describe "Redbeard" do
         it 'should accept an Info Hash' do
             get '/announce', {:INFO_HASH => '640FE84C613C17F663551D218689A64E8AEBEABE'}
             last_request.params['INFO_HASH'].should == '640FE84C613C17F663551D218689A64E8AEBEABE'
+            # last_request.params['INFO_HASH'].bytesize.should == 20 * 2
+        end
+
+        it 'should reject an Info Hash that is not 20 bytes' do
+            get '/announce', {:INFO_HASH => '640FE84C613C17F663551D218689A64E8AEBEABEDEADBEEF'}
+            last_response.should_not be_ok
         end
     end
 end
