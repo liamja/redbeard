811429fe1002e1e64cff122a739ee8873fb0e7c4
Liam Anderson <Liam.Anderson.91@gmail.com>
2013-08-04 22:56:54 +0100

    Measure info_hash by byte size, not char length.

--- lib/redbeard/http.rb
+++ lib/redbeard/http.rb
@@ -3,7 +3,7 @@ require 'uri'
 
 get '/announce' do
     unless params['INFO_HASH'].nil?
-        if params['INFO_HASH'].length != 40 
+        if params['INFO_HASH'].bytesize != 40 
             raise 'INFO_HASH is not 20 bytes long.'
         end
     end
